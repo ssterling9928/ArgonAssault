3ff246e7df2d6590fa39f580a6ec50f243b9ca1a
Stephen Sterling <ssterling9928@yahoo.com>
2021-08-14 14:37:25 -0400

    Finish Player Movement /  Add to timeline.

--- Assets/PlayerControls.cs
+++ Assets/PlayerControls.cs
@@ -8,12 +8,46 @@ public class PlayerControls : MonoBehaviour
     [SerializeField] private float xRange = 5f;
     [SerializeField] private Vector2 yRange;
 
+    [SerializeField] float positionPitchFactor = -2f;
+    [SerializeField] float controlPitichFactor = -10f;
+
+    [SerializeField] float positionYawFactor = 2f;
+    [SerializeField] float controlYawFactor = 10f;
+    [SerializeField] float controlRollFactor = 5f;
+
+
+    float horizontalThrow, verticalThrow;
+
     // Update is called once per frame
     void Update()
     {
-        var horizontalThrow = Input.GetAxis("Horizontal");
-        var verticalThrow = Input.GetAxis("Vertical");
-        
+        ProcessTranslation();
+        ProcessRotation();
+
+    }
+
+
+    private void ProcessRotation() 
+    {
+
+        float pitchDueToPosition = transform.localPosition.y * positionPitchFactor;
+        float pitchDueToControlThrow = verticalThrow * controlPitichFactor;
+        float pitch = pitchDueToPosition + pitchDueToControlThrow;
+
+        float yawDueToPosition = transform.localPosition.x * positionYawFactor;
+        float yawDueToControl = horizontalThrow * controlYawFactor;
+        float yaw = yawDueToPosition + yawDueToControl;
+
+
+        float roll = horizontalThrow * controlRollFactor;
+        transform.localRotation = Quaternion.Euler(pitch, yaw, roll);
+    }
+
+    private void ProcessTranslation()
+    {
+        horizontalThrow = Input.GetAxis("Horizontal");
+        verticalThrow = Input.GetAxis("Vertical");
+
         var xOffset = horizontalThrow * Time.deltaTime * controlSpeed;
         var newXPos = transform.localPosition.x + xOffset;
         var clampedXPos = Mathf.Clamp(newXPos, -xRange, xRange);
@@ -25,8 +59,5 @@ public class PlayerControls : MonoBehaviour
 
 
         transform.localPosition = new Vector3(clampedXPos, clampedYPos, transform.localPosition.z);
-
-
-        
     }
 }
